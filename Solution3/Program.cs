5267325f4a503aa8585224b49bf2638c2c369d6b
Evgeny Petrov <petroves88@mail.ru>
2022-10-15 15:43:36 +0300

    Solution3

---
+++ Solution3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA%2==0){
+    Console.WriteLine("Да");
+} else
+{
+    Console.WriteLine("Нет");
+}
