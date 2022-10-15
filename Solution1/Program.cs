a407cadea12476fd336872b6f1622992fd82b8e1
Evgeny Petrov <petroves88@mail.ru>
2022-10-15 15:30:05 +0300

    Solution1

---
+++ Solution1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine("max = " + numberA);
+}
+else if (numberA < numberB)
+{
+    Console.WriteLine("max = " + numberB);
+}
+else
+{
+Console.WriteLine("Числа равны");
+}
