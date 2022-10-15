8754fad19b329770cd8e900db1c1f252538e78ef
Evgeny Petrov <petroves88@mail.ru>
2022-10-15 15:39:47 +0300

    Solution2

---
+++ Solution2/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    if (numberA > numberC)
+    {
+        Console.WriteLine("max = " + numberA);
+    }
+    else
+    {
+        Console.WriteLine("max = " + numberC);
+    }
+}
+else if (numberB > numberA)
+{
+    if (numberB > numberC)
+    {
+        Console.WriteLine("max = " + numberB);
+    }
+    else
+    {
+        Console.WriteLine("max = " + numberC);
+    }
+
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
