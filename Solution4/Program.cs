7cafadd6e60b4655ab2d1ee17967b9ed7ae1fdcd
Evgeny Petrov <petroves88@mail.ru>
2022-10-15 15:52:31 +0300

    Solution4

---
+++ Solution4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+while (count <= numberA)
+{
+    Console.Write(count + " ");
+    count += 2;
+}
